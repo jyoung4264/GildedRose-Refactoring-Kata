814947be6f63bcfe4da2599ac50d332a12babd58
Jenny Young <jennyyoung@Jennys-MacBook-Air.local>
2023-08-11 10:30:30 +0100

    Correct broken test and add test to compare current output to expected output

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -6,8 +6,47 @@ describe GildedRose do
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+      expect(items[0].name).to eq "foo"
     end
   end
 
+  describe "expected result" do
+    before do
+      items = [
+        Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20),
+        Item.new(name="Aged Brie", sell_in=2, quality=0),
+        Item.new(name="Elixir of the Mongoose", sell_in=5, quality=7),
+        Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+        Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+        Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+        Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+        Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+        # This Conjured item does not work properly yet
+        Item.new(name="Conjured Mana Cake", sell_in=3, quality=6), # <-- :O
+      ]
+      days = 31
+      test_output = File.new("ruby/test_output.txt", 'w')
+      test_output.write("OMGHAI!")
+      test_output.write( "\n" )
+      gilded_rose = GildedRose.new items
+        (0...days).each do |day|
+          test_output.write( "-------- day #{day} --------\n")
+          test_output.write( "name, sellIn, quality\n" )
+          items.each do |item|
+            test_output.write( item )
+            test_output.write( "\n" )
+          end
+          test_output.write( "\n")
+          gilded_rose.update_quality
+        end
+        test_output.close()
+    end
+    it "matches expected output" do
+      standard_output = File.open("texttests/ThirtyDays/stdout.gr").read()
+      new_output = File.open("ruby/test_output.txt").read()
+      puts new_output
+      expect(new_output).to eq(standard_output)
+    end
+  end
 end
+
