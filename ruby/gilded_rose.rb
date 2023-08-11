95244b021d6ba2fcdabae93335a6c02268f3a457
Jenny Young <jennyyoung@Jennys-MacBook-Air.local>
2023-08-11 11:20:04 +0100

    start moving logic into separate methods

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -4,28 +4,38 @@ class GildedRose
     @items = items
   end
 
+  def ordinary_item(item)
+     item.quality -= 1 if item.quality > 0
+  end
+
+  def aged_brie(item)
+     item.quality += 1 if item.quality < 50
+  end
+
+  def backstage_pass(item)
+    if item.sell_in < 6
+      if item.quality < 50
+        item.quality = item.quality + 1
+      end
+    end
+    if item.sell_in < 11
+      if item.quality < 50
+        item.quality = item.quality + 1
+      end
+    end
+  end
+
   def update_quality()
     @items.each do |item|
       next if item.name.eql? "Sulfuras, Hand of Ragnaros"
-      
+
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-            item.quality = item.quality - 1
-        end
+        ordinary_item(item)
       else
         if item.quality < 50
           item.quality = item.quality + 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
+            backstage_pass(item)
           end
         end
       end
@@ -33,9 +43,7 @@ class GildedRose
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              item.quality = item.quality - 1
-            end
+            ordinary_item(item)
           else
             item.quality = item.quality - item.quality
           end
